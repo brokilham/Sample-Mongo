a6a7fc695cdecbc0b2e032e1b50bb1fd3561d2fe
ilham agung <plato.ilham@gmail.com>
2018-07-13 14:50:53 +0700

    first init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Mongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase("myNewDB”");
+            Author author = new Author
+            {
+                Id = 1,
+                FirstName = "Joydip",
+                LastName = "Kanjilal"
+            };
+            var collection = db.GetCollection<Author>("authors");
+            collection.InsertOne(author);
+            Console.Read();
+        }
+
+        
+    }
+}
